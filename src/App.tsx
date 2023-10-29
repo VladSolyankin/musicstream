10578eb9d88cdbff09de57abdc4841628558e0bc
VladSolyankin <darnell.dewberry@mail.ru>
2023-10-30 00:47:51 +0300

    added components to the main component and changed page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
 import "./css/App.css"
+import StartPageHero from "./components/StartPageHero.tsx";
+import SongVariationsCards from "./components/SongVariationsCards.tsx";
 
 function App() {
 
   return (
     <div className="">
-      <Header />
+        <Header />
+        <StartPageHero />
+        <SongVariationsCards />
     </div>
   )
 }
