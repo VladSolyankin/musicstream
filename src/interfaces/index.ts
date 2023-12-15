faefaf8abc3241889b94838c7c05ea455976dd14
VladSolyankin <darnell.dewberry@mail.ru>
2023-12-15 21:29:32 +0300

    Added interfaces folder with some interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,20 @@
+export interface Track {
+    id: string,
+    album: Album,
+    name: string,
+    message: string,
+    preview_url?: string,
+    artists: Array<Artist>
+}
+
+interface Artist {
+    name: string
+}
+
+interface Album {
+    images: Array<Image>
+}
+
+interface Image {
+    url: string
+}
