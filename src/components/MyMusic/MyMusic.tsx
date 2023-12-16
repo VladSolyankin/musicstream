12429dbe0d3b230882bb693b99e1642a6f49c3d1
VladSolyankin <darnell.dewberry@mail.ru>
2023-12-16 22:58:21 +0300

    Changed playlist selection logic (rendering sidebar menu onClick)

--- src/components/MyMusic/MyMusic.tsx
+++ src/components/MyMusic/MyMusic.tsx
@@ -2,14 +2,22 @@ import Footer from "../Footer.tsx";
 import Header from "./Header.tsx";
 import Playlists from "./Playlists.tsx";
 import LikedPlaylists from "./LikedPlaylists.tsx";
+import SelectedPlaylist from "./SelectedPlaylist.tsx";
+import {useState} from "react";
 
 const MyMusic = () => {
+    const [isPlaylistSelected, setIsPlaylistSelected] = useState(false)
+    const onPlaylistSelect = () => {
+        setIsPlaylistSelected(!isPlaylistSelected)
+        console.log('clicked')
+    }
     return (
         <div>
             <Header />
-            <Playlists />
+            <Playlists onPlaylistSelect={onPlaylistSelect}/>
             <LikedPlaylists />
             <Footer />
+            {isPlaylistSelected && <SelectedPlaylist isVisible={isPlaylistSelected}/>}
         </div>
     );
 };
