4be5dc095fa585a0bd6b3f9245c20275f7a0634d
VladSolyankin <darnell.dewberry@mail.ru>
2023-12-19 10:46:14 +0300

    Changed SelectedPlaylist.tsx structure and styling, added play button images

--- src/components/MyMusic/SelectedPlaylist.tsx
+++ src/components/MyMusic/SelectedPlaylist.tsx
@@ -2,13 +2,38 @@ import React from 'react';
 import '../../css/Sidemenu.css'
 
 interface SelectedPlaylistProps {
-	isVisible: boolean
+	isVisible: boolean,
+	imagePath: string,
+	title: string
 }
 
-const SelectedPlaylist: React.FC<SelectedPlaylistProps> = ({isVisible}) => {
+const SelectedPlaylist: React.FC<SelectedPlaylistProps> = ({isVisible, imagePath, title}) => {
+
+	const onMenuClosed = (event: React.MouseEvent<HTMLDivElement>) => {
+		event.stopPropagation()
+		const menuClassList = event.currentTarget.parentElement?.classList;
+		menuClassList.replace("wrapper", "menu__closed")
+	}
 	return (
-		<div className={isVisible ? "wrapper menu__opened" : "menu__closed"}>
-			
+		<div className={isVisible ? "wrapper" : "menu__closed"}>
+			<div className="menu__background" onClick={onMenuClosed}></div>
+			<div className="menu__opened">
+				<div className="flex justify-between">
+					<div>
+						<h2>ПЛЕЙЛИСТ</h2>
+						<h3>{title}</h3>
+					</div>
+					<img src={imagePath} alt="Playlist image" className="h-12 w-12"/>
+				</div>
+
+				<button className="flex justify-center min-w-full">
+					<img src="./src/assets/playlist_play_button.png" alt="" className="w-20 h-20"/>
+				</button>
+
+				<ol className="list-disc">
+					<li>hello</li>
+				</ol>
+			</div>
 		</div>
 	);
 };
