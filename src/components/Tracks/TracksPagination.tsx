2e94692be156973d52dec7148a7cfa8a609905f1
VladSolyankin <darnell.dewberry@mail.ru>
2023-12-23 16:45:57 +0300

    Added TracksPagination component

---
+++ src/components/Tracks/TracksPagination.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const TracksPagination = ({onPageChange}) => {
+	return (
+		<ul className="flex justify-between gap-10 text-white">
+			{
+				Array.from({length: 9}, (_v, index) => (
+					<li key={index}
+						className="border-white border-2 p-4 px-5 font-bold text-2xl hover:bg-gray-600"
+						onClick={() => onPageChange(index)}
+					>
+						{index + 1}
+					</li>
+				))
+			}
+		</ul>
+	);
+};
+
+export default TracksPagination;
