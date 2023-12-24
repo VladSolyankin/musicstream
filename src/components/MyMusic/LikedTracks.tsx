01bbf320a779856e957839866d78d48dfed5aaaa
VladSolyankin <darnell.dewberry@mail.ru>
2023-12-24 19:25:06 +0300

    Refactoring LikedTracks.tsx

--- src/components/MyMusic/LikedTracks.tsx
+++ src/components/MyMusic/LikedTracks.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import LikedTrackList from "./LikedTrackList.tsx";
 
-const LikedTracks = ({onLikeClick, likedTracksIds, likedTracks}) => {
+const LikedTracks = () => {
+
+
 
     return (
         <div className="flex flex-col justify-center items-center border-2 border-white max-w-5xl mx-auto my-10 py-10 rounded-xl bg-gray-12 p-10">
@@ -13,16 +16,7 @@ const LikedTracks = ({onLikeClick, likedTracksIds, likedTracks}) => {
                 </h2>
             </div>
             <div className="flex flex-col">
-                {likedTracks.map(track => (
-                    <li key={track.id} className="border p-4 mb-4 text-white flex items-center justify-between gap-10">
-                        <img src={track.album.images[2].url} alt="" className="basis-1/8"/>
-                        <span className="text-center text-white basis-1/4">{track.name + " - " + track.artists[0].name}</span>
-                        <audio controls src={track.preview_url} className="bg-white basis-2/4"/>
-                        <img onClick={() => onLikeClick(track.id)} src={
-                            likedTracksIds.includes(track.id) ? "src/assets/liked.png" : "src/assets/unliked.png"
-                        } alt="" className="w-8 h-7"/>
-                    </li>
-                ))}
+                <LikedTrackList />
             </div>
         </div>
     );
