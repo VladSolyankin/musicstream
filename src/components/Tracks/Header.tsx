bf270e9facd03cdd386400bb6205c05738dc3d42
VladSolyankin <darnell.dewberry@mail.ru>
2023-12-26 19:33:27 +0300

    Changed Tracks.tsx header styles

--- src/components/Tracks/Header.tsx
+++ src/components/Tracks/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
 	const navigator = useNavigate();
 	return (
 		<div className="flex justify-center items-center mx-auto max-w-7xl">
-			<div className="flex justify-start items-center basis-3/4 h-16 bg-gray-900 rounded-xl bg-gray-12 my-4 p-4">
+			<div className="flex justify-start items-center basis-3/4 h-16 rounded-xl bg-gray-12 my-4 p-4">
 				<img onClick={() => navigator("/")}
 					src="src/assets/logo-transparent.png" className="w-36 cursor-pointer" alt="Musicstream logo"/>
 				<ul className="flex items-center justify-between basis-4/6 text-white text-lg font-jost mx-auto">
