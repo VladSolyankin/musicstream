e1191a77da9e6273c5893ca29ff7e04353740323
VladSolyankin <darnell.dewberry@mail.ru>
2023-12-27 15:03:09 +0300

    Updated Creators.tsx and added CreatorsList.tsx

--- src/components/Creators/Creators.tsx
+++ src/components/Creators/Creators.tsx
@@ -1,8 +1,13 @@
+import Header from "../Header";
+import Footer from "../Footer";
+import CreatorsList from "./CreatorsList";
 
 const Creators = () => {
     return (
-        <div>
-
+        <div className="flex flex-col justify-between min-h-screen">
+            <Header />
+            <CreatorsList />
+            <Footer />
         </div>
     );
 };
---
+++ src/components/Creators/CreatorsList.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {getArtistById, getArtistsIdsByLetter} from "../../js/spotifyAPI";
+
+const CreatorsList: React.FC = () => {
+
+	const getAllEnglishLetters = () => {
+		const alphabet = []
+		const firstLetter = 'A'.charCodeAt(0)
+		const lastLetter = 'Z'.charCodeAt(0)
+
+		for (let i = firstLetter; i <= lastLetter; i++) {
+			alphabet.push(String.fromCharCode(i))
+		}
+
+		return alphabet
+	}
+
+	//console.log(getArtistsIdsByLetter('a'))
+
+	return (
+		<div className="flex flex-col items-center gap-10">
+			{
+				getAllEnglishLetters().map(letter => (
+					<ul className="columns-3" key={letter + letter.id}>
+						<h2 className="text-white">{letter}</h2>
+						<div>
+							{
+								//getArtistById(getArtistsIdsByLetter(letter))
+							}
+						</div>
+					</ul>
+				))
+			}
+		</div>
+	);
+};
+
+export default CreatorsList;
