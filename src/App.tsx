9e38ecc373db3510e47a0db09219e329e342f6ab
VladSolyankin <darnell.dewberry@mail.ru>
2024-01-07 18:12:10 +0300

    Renamed Creators.tsx to Artists.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import MainPage from "./components/MainPage/MainPage.tsx";
 import MyMusic from "./components/MyMusic/MyMusic.tsx";
 import Tracks from "./components/Tracks/Tracks.tsx";
-import Creators from "./components/Creators/Creators.tsx";
+import Artists from "./components/Artists/Artists.tsx";
 import Other from "./components/Other/Other.tsx";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
                 <Route path="/" element={<MainPage/>}></Route>
                 <Route path="/my_music" element={<MyMusic/>}></Route>
                 <Route path="/tracks" element={<Tracks/>}></Route>
-                <Route path="/creators" element={<Creators/>}></Route>
+                <Route path="/creators" element={<Artists/>}></Route>
                 <Route path="/other" element={<Other/>}></Route>
             </Routes>
         </BrowserRouter>
