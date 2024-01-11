f57e2a9f16c35ad4876446e6d960abf4217e9872
VladSolyankin <darnell.dewberry@mail.ru>
2024-01-11 23:59:26 +0300

    Fixed artist top tracks rendering

--- src/components/Tracks/SearchedTracks.tsx
+++ src/components/Tracks/SearchedTracks.tsx
@@ -7,7 +7,7 @@ const SearchedTracks = ({tracks, onLikeClick, likedTracksIds}) => {
 	return (
 		<div className="my-5">
 			<ol className="flex-col">
-				{tracks.map((track: Track) => (
+				{tracks.items.map((track: Track) => (
 					<li key={track.id} className="border p-4 mb-4 text-white flex items-center justify-between gap-10">
 						<img src={track.album.images[2].url} alt="" className="basis-1/8"/>
 						<span className="text-center text-white basis-1/4">{track.name + " - " + track.artists[0].name}</span>
