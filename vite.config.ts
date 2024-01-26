5289c27aed8f07be179f3749f264c5647f18e592
VladSolyankin <darnell.dewberry@mail.ru>
2024-01-26 15:47:11 +0300

    Changed configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/src/main.tsx',
-    },
-  },
   server: {
     proxy: {
       "/getArtists": {
@@ -31,5 +24,5 @@ export default defineConfig({
     }
   },
   plugins: [react()],
-  base: '/'
+  base: "/"
 })
